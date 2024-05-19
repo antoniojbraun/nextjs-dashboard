624756bd7695193fbd383c0c45083ab1bc686861
Antonio J. Braun <antoniojbraun@gmail.com>
2024-05-19 16:49:36 -0300

    feito até o capitulo 15

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,56 @@
+// import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react'; // FallBack Component
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+  CardsSkeleton
+} from '@/app/ui/skeletons'; // FallBack Component
+import {
+  // fetchRevenue,
+  // fetchLatestInvoices,
+  // fetchCardData,
+} from '@/app/lib/data';
+
+export default async function Page() {
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices(); // Executa só quando fetchRevenue finaliza.
+  // const {
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  // } = await fetchCardData(); // Executa só quando fetchLatestInvoices finaliza.
+  return (
+    <main>
+      <h1 className={`${lusitana.className} text-x1 md:text-2x1 mb-4`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        /> */}
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        {/* <RevenueChart revenue={revenue} /> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,35 @@
-'use client';
+'use client'; // componente de cliente.
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import {
+  useSearchParams,
+  useRouter,
+  usePathname,
+} from '@/node_modules/next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // Aqui abaixo usamos a função useDebouncedCallback com nossa função handleSearch para tratar a digitação
+  // demasiada do teclado do usuário.
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+
+    // aqui estamos verificando se existe alguma informação no campo de busca, caso houver ele vai setar na instancia
+    // de URLSearchParams, caso não houver, ele deleta tudo.
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+
+    replace(`${pathname}?${params.toString()}`); // aqui ele vai pegar o endereço da URL + o contéudo que está em params e jogar na URL.
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +38,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query'?.toString)} // aqui ele pega o que está parametro query e transforma em texto.
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
